99da6bb9c496b914effb605896aebf173b5564de
Krzysztof Szklarski <kszklarski7@gmail.com>
2025-11-12 22:21:19 +0100

    upped the css version so no cache

--- src/Base/Enqueue.php
+++ src/Base/Enqueue.php
@@ -13,7 +13,9 @@ class Enqueue extends BaseController
     public function enqueue_public_styles(){
         wp_enqueue_style(
             'donations_for_paynow_styles',
-            $this->plugin_url .  'assets/public-styles.css'
+            $this->plugin_url .  'assets/public-styles.css',
+            [],
+            '1.1'
         );
     }
 
